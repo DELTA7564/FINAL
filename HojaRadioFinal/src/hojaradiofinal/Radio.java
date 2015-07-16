8714702a4fd60a45fd081e19dcb15b22d0d54df6
Belen <mariabelenhb@gmail.com>
2015-07-15 22:17:51 -0600

    Javadoc

--- HojaRadioFinal/src/hojaradiofinal/Radio.java
+++ HojaRadioFinal/src/hojaradiofinal/Radio.java
@@ -20,20 +20,52 @@ package hojaradiofinal;
  */
 public interface Radio {
    
-        public void setESTADO(boolean nEstado); //controla el estado del radio ON/OFF dependiendo del parametro
+    /**
+     *
+     * @param nEstado
+     */
+    public void setESTADO(boolean nEstado); //controla el estado del radio ON/OFF dependiendo del parametro
     
+    /**
+     *
+     * @return
+     */
     public boolean getEstado(); //getter para el estado, envia el estado actual
     
+    /**
+     *
+     * @return
+     */
     public int getAMFM(); //getter para la banda AM/FM, envia la banda actual
     
+    /**
+     *
+     * @param banda
+     */
     public void setAMFM(int banda); //controla la banda de la radio AM/FM dependiendo del parametro
     
+    /**
+     *
+     * @param ud
+     */
     public void Sintonizar(boolean ud); //controla las emisoras, avanza o retrocede segun el parametro ud
     
+    /**
+     *
+     * @param pos
+     */
     public void Guardar(int pos); //se encarga de almacenar en la memoria el contenido de cada boton
     
+    /**
+     *
+     * @param pos
+     */
     public void Memoria(int pos); //se encarga de cargar (dependiendo de la posicion y del la banda) la estacion del boton seleccionado
     
+    /**
+     *
+     * @return
+     */
     public double getEmisora(); //getter para la estacion actual
     
 }
